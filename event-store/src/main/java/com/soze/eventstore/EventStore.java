3d7b280651477f663803d58f052d0a6eaf67f6ee
sozemego <piknypiec@gmail.com>
2018-06-15 13:19:12 +0200

    Not-working properties

--- event-store/src/main/java/com/soze/eventstore/EventStore.java
+++ event-store/src/main/java/com/soze/eventstore/EventStore.java
@@ -1,9 +1,22 @@
 package com.soze.eventstore;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.soze.events.BaseEvent;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StreamUtils;
 
+import javax.annotation.PostConstruct;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
@@ -14,6 +27,14 @@ import static com.soze.events.BaseEvent.*;
 public class EventStore {
 
   private final Queue<BaseEvent> events = new ConcurrentLinkedQueue<>();
+
+  @Value("classpath:events.json")
+  private Resource eventsFile;
+
+  @PostConstruct
+  public void setup() throws Exception {
+    loadEvents();
+  }
 
   public List<BaseEvent> getAggregateEvents(UUID aggregateId, boolean latest) {
     try {
@@ -48,6 +69,49 @@ public class EventStore {
   public void handleMessage(BaseEvent event) {
     System.out.println(event);
     events.add(event);
+    persist();
+  }
+
+  private void persist() {
+    try {
+      File file = new ClassPathResource("events.json").getFile();
+      FileSystemUtils.deleteRecursively(file);
+      file.createNewFile();
+
+      final String json = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(events);
+      FileWriter writer = new FileWriter(file);
+      FileCopyUtils.copy(json, writer);
+
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void loadEvents() throws IOException {
+    System.out.println("READING EVENTS");
+    final String json = StreamUtils.copyToString(
+      eventsFile.getInputStream(),
+      Charset.defaultCharset()) + "\n";
+
+    List<BaseEvent> baseEvents = new ArrayList<>();
+    final ObjectMapper mapper = new ObjectMapper();
+    try {
+      baseEvents = mapper.readValue(
+        json,
+        mapper.getTypeFactory().constructCollectionType(List.class, BaseEvent.class)
+      );
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    baseEvents
+      .stream()
+      .sorted(Comparator.comparing(BaseEvent::getCreatedAt))
+      .peek(System.out::println)
+      .forEach(events::add);
+    System.out.println("READ EVENTS");
   }
 
 }
