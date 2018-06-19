12b6b2a4ac1a471d99d4fcb350cf24a5015885ce
sozemego <piknypiec@gmail.com>
2018-06-19 15:07:48 +0200

    Added collection utils

---
+++ common/src/main/java/com/soze/common/utils/CollectionUtils.java
@@ -0,0 +1,26 @@
+package com.soze.common.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionUtils {
+
+  /**
+   * Returns true if set1 contains any of the elements in set2.
+   * @param set1
+   * @param set2
+   * @return
+   */
+  public static boolean containsAny(Set<?> set1, Set<?> set2) {
+    return set1.retainAll(set2);
+  }
+
+  public static <E> Set<E> setOf(E... elements) {
+    Set<E> set = new HashSet<>(elements.length);
+    for (final E element : elements) {
+      set.add(element);
+    }
+    return set;
+  }
+
+}
