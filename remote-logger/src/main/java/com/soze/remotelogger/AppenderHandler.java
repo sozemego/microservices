7a78370613e8ca37f0b15cadff4c3091b524dade
sozemego <piknypiec@gmail.com>
2018-06-21 12:15:36 +0200

    Remote logging fix

--- remote-logger/src/main/java/com/soze/remotelogger/AppenderHandler.java
+++ remote-logger/src/main/java/com/soze/remotelogger/AppenderHandler.java
@@ -4,13 +4,19 @@ import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.boolex.OnMarkerEvaluator;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.FileAppender;
+import ch.qos.logback.core.boolex.EvaluationException;
+import ch.qos.logback.core.boolex.EventEvaluatorBase;
 import ch.qos.logback.core.filter.EvaluatorFilter;
 import ch.qos.logback.core.spi.FilterReply;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * For each application id, adds new file appender.
@@ -25,9 +31,10 @@ public class AppenderHandler {
   public void handleAppender(String applicationId, Marker marker) {
 
     LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-    String appenderName = applicationId + "-" + marker.getName() + "-" + APPENDER_SUFFIX;
+    String appenderName = applicationId + "-" + marker.getName() + APPENDER_SUFFIX;
     Logger logger = loggerContext.getLogger("ROOT");
     Appender appender = logger.getAppender(appenderName);
+
     if(appender != null) {
       return;
     }
@@ -42,18 +49,54 @@ public class AppenderHandler {
     encoder.setPattern("%msg");
     encoder.start();
     fileAppender.setEncoder(encoder);
-    fileAppender.start();
 
     EvaluatorFilter filter = new EvaluatorFilter();
     filter.setOnMismatch(FilterReply.DENY);
     filter.setOnMatch(FilterReply.ACCEPT);
 
-    OnMarkerEvaluator onMarkerEvaluator = new OnMarkerEvaluator();
-    marker.iterator().forEachRemaining(m -> onMarkerEvaluator.addMarker(m.getName()));
+    MultipleMarkerEvaluator onMarkerEvaluator = new MultipleMarkerEvaluator();
+    onMarkerEvaluator.addMarker(marker.getName());
+    marker
+      .iterator()
+      .forEachRemaining(child -> {
+        onMarkerEvaluator.addMarker(child.getName());
+      });
     filter.setEvaluator(onMarkerEvaluator);
-    fileAppender.addFilter(filter);
+    onMarkerEvaluator.start();
 
+    filter.start();
+    fileAppender.addFilter(filter);
+    fileAppender.start();
     logger.addAppender(fileAppender);
+  }
+
+  private static class MultipleMarkerEvaluator extends EventEvaluatorBase<ILoggingEvent> {
+
+    private final List<String> markerList = new ArrayList<String>();
+
+    public void addMarker(String markerStr) {
+      markerList.add(markerStr);
+    }
+
+
+    /**
+     * Return true if event passed as parameter all of the specified markers.
+     */
+    public boolean evaluate(ILoggingEvent event) throws NullPointerException, EvaluationException {
+
+      Marker eventsMarker = event.getMarker();
+      if (eventsMarker == null) {
+        return false;
+      }
+
+      for (String markerStr : markerList) {
+        if (!eventsMarker.contains(markerStr)) {
+          return false;
+        }
+      }
+      return true;
+    }
+
   }
 
 }
--- remote-logger/src/main/java/com/soze/remotelogger/LogHandler.java
+++ remote-logger/src/main/java/com/soze/remotelogger/LogHandler.java
@@ -3,7 +3,6 @@ package com.soze.remotelogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.stereotype.Service;
