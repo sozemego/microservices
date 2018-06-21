d287aecac884e91aed95e9bd4b19751fa6b0c9fd
sozemego <piknypiec@gmail.com>
2018-06-21 12:18:07 +0200

    Logback fix, imported IncomingRequestLogger

--- users/src/main/java/com/soze/users/Config.java
+++ users/src/main/java/com/soze/users/Config.java
@@ -3,12 +3,14 @@ package com.soze.users;
 import com.soze.common.aop.CommandAspect;
 import com.soze.common.repository.SourcedRepository;
 import com.soze.common.repository.SourcedRepositoryImpl;
+import com.soze.common.rest.IncomingRequestLogger;
 import com.soze.common.service.*;
 import com.soze.users.aggregate.User;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -17,14 +19,18 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Import({CommandAspect.class})
+@Import({CommandAspect.class, IncomingRequestLogger.class})
 public class Config implements WebMvcConfigurer {
 
   public static final String QUEUE = "USER_QUEUE";
   public static final String EXCHANGE = "EXCHANGE";
+
+  @Autowired
+  private IncomingRequestLogger incomingRequestLogger;
 
   @Bean
   Queue queue() {
@@ -87,4 +93,8 @@ public class Config implements WebMvcConfigurer {
     return new SourcedRepositoryImpl<>(User.class, eventStoreService);
   }
 
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(incomingRequestLogger);
+  }
 }
