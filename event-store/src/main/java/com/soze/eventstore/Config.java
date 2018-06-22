3a12b7609e3b7cfd5e88c38050bdf2c0e73a0424
sozemego <piknypiec@gmail.com>
2018-06-22 08:09:57 +0200

    Logging to eventstore

--- event-store/src/main/java/com/soze/eventstore/Config.java
+++ event-store/src/main/java/com/soze/eventstore/Config.java
@@ -1,21 +1,29 @@
 package com.soze.eventstore;
 
+import com.soze.common.rest.IncomingRequestLogger;
 import com.soze.common.service.EventPublisherService;
 import com.soze.common.service.EventPublisherServiceImpl;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Import({IncomingRequestLogger.class})
 public class Config implements WebMvcConfigurer {
 
   public static final String QUEUE = "EVENT_STORE_QUEUE";
   public static final String EXCHANGE = "EXCHANGE";
+
+  @Autowired
+  private IncomingRequestLogger incomingRequestLogger;
 
   @Bean
   Queue queue() {
@@ -47,6 +55,11 @@ public class Config implements WebMvcConfigurer {
   @Bean
   EventPublisherService eventPublisherService(RabbitTemplate rabbitTemplate) {
     return new EventPublisherServiceImpl(rabbitTemplate);
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(incomingRequestLogger);
   }
 
 }
